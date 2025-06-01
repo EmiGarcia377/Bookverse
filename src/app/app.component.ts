344d657e6f6a602e4bd2efc878ea0a46565f3356
Emi Garcia <cosas.unity@gmail.com>
2025-05-31 19:59:25 -0600

    Se creo apartado de reseñas y tambien pie de pagina. Con esto se acaba el fron-end de la pagina de inicio de la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CardsSliderComponent } from "./components/cards-slider/cards-slider.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardsSliderComponent],
+  imports: [RouterOutlet, HeaderComponent, CardsSliderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/cards-slider/cards-slider.component.ts
+++ src/app/components/cards-slider/cards-slider.component.ts
@@ -1,42 +1,20 @@
 import { Component } from '@angular/core';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { animate, scroll } from 'motion'
 
 @Component({
   selector: 'app-cards-slider',
-  imports: [SlickCarouselModule],
+  imports: [],
   templateUrl: './cards-slider.component.html',
   styleUrl: './cards-slider.component.css'
 })
 export class CardsSliderComponent {
- slides = [
-  {img: 'ElPoderDelAhora.webp'},
-  {img: 'ElPrincipe.webp'},
-  {img: 'NoMePuedesLastimar.webp'}
- ];
-
- slideConfig = {
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  readonly container = document.querySelector('.img-group') as HTMLElement;
+ ngAfterViewInit(){
+  scroll(
+    animate(".img-group", {
+      transform: ["none", `translateX(-200vw)`],
+    }),
+    { target: document.querySelector("section") ?? undefined }
+ );
  }
 }
